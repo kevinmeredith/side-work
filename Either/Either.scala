94ff3bae9fc67d44f650799c8164c305e76b759c
unknown <ejai2xl@ACNU3299MQS.na.corp.mckesson.com>
2013-09-12 23:17:09 -0400

    added flatMap for Either

--- Either/Either.scala
+++ Either/Either.scala
@@ -3,6 +3,11 @@ sealed trait Either[+E, +A] {
 		case Right(y) => Right(f(y))
 		case Left(e) => Left(e)
 	}
+
+	def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match { 
+		case Right(y) => f(y)
+		case Left(e) => Left(e)
+	}
 }
 
 case class Left[+E](err: E) extends Either[E,Nothing]
