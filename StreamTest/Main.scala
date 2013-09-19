469009ac3f8838b32dced13c0457cfffea0db645
unknown <ejai2xl@ACNU3299MQS.na.corp.mckesson.com>
2013-09-18 23:44:01 -0400

    added Stream testing

---
+++ StreamTest/Main.scala
@@ -0,0 +1,12 @@
+import scala.math.BigInt
+object Main extends App {
+	
+	val fibs: Stream[BigInt] = BigInt(0) #:: BigInt(1) #:: 
+		fibs.zip(fibs.tail).map(n => {
+			println("Adding %d and %d".format(n._1, n._2))
+			n._1 + n._2
+		})
+
+	fibs take 5 foreach println
+	//fibs take 6 foreach println
+}
