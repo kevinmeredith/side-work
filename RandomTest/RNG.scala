82dd2ca82d245f73b19273eed7f60a7314389e23
unknown <ejai2xl@ACNU3299MQS.na.corp.mckesson.com>
2013-09-19 23:23:33 -0400

    making stateful APIs pure chapter

---
+++ RandomTest/RNG.scala
@@ -0,0 +1,16 @@
+package fp
+
+trait RNG {
+	def nextInt: (Int, RNG)
+}
+
+object RNG {
+	def simple(seed: Long): RNG = new RNG {
+		def nextInt: (Int, RNG) = {
+			val seed2 = (seed*0x5DEECE66DL + 0xBL) &
+						((1L << 48) - 1)
+			((seed2 >>> 16).asInstanceOf[Int],
+				simple(seed2))
+		}
+	}
+}
---
+++ RandomTest/positiveInt.scala
@@ -0,0 +1,9 @@
+package fp
+
+// EXERCISE 1: Write a function that uses RNG.nextInt to generate 
+// a random positive integer. Make sure to handle the corner case 
+// when nextInt returns Int.MinValue, which doesn't have a positive 
+// counterpart.
+def positiveInt(rng: RNG): (Int, RNG) = {
+	
+}
