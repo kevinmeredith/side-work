7821ca773c6b6f305412ef57e7905282877332fc
kman007us <kevin.m.meredith@gmail.com>
2013-09-29 12:17:30 -0400

    added function to print a list of random ints based on a given count

--- RandomTest/fp/Test.scala
+++ RandomTest/fp/Test.scala
@@ -1,36 +1,43 @@
 object Test {
 	
   	def main(args: Array[String]) {
-		def verifyPositiveInt(i: Int) : Unit = {
-			val rng = RNG.simple(i)
-			val (r, _) = rng.nextInt
-			val (posR, _) = positiveInt(rng)
-			
-			r match {
-				case x if x < 0 => println("[" + i + "] , r: " + r + " and positive r : " + posR)
-				case _ => ()
-			}
-  		}
+		val list = (-1000 to 5000).toList
+		val rng = RNG.simple(42)
+		val (results, rng2) = ints(10)(rng)
 
-		// EXERCISE 2: Write a function to generate a Double between 0 and 1, not
-		// including 1. Note: you can use Int.MaxValue to obtain the maximum positive
-		// integer value and you can use x.toDouble to convert an Int, x, to a Double. 
-		// (www.manning.com/bjarnason/)
-  		def double(rng: RNG): (Double, RNG) = {
-			val (r, _) = rng.nextInt
-			val d: Double = ( math.abs(r).toDouble / Int.MaxValue ) match {
-				case 1 => .99999
-				case x: Double => x
-			}
-			(d, rng)
-  		}
+		println("results: " + results)
 
-  		val list = (-1000 to 5000).toList
-  		val rng = RNG.simple(42)
-  		val (d, _) = double(rng)
-  		println("d: " + d)
-  		//list.map(x => verifyPositiveInt(x))  		
+		//val((i,d), rng2) = intDouble(rng)
+		//println("i: " + i + ", d: " + d)
+
+		//val (d, _) = double(rng)
+		//println("d: " + d)
+		//list.map(x => verifyPositiveInt(x))  		
 	}
+
+	def verifyPositiveInt(i: Int) : Unit = {
+		val rng = RNG.simple(i)
+		val (r, _) = rng.nextInt
+		val (posR, _) = positiveInt(rng)
+		
+		r match {
+			case x if x < 0 => println("[" + i + "] , r: " + r + " and positive r : " + posR)
+			case _ => ()
+		}
+	}
+
+	// EXERCISE 2: Write a function to generate a Double between 0 and 1, not
+	// including 1. Note: you can use Int.MaxValue to obtain the maximum positive
+	// integer value and you can use x.toDouble to convert an Int, x, to a Double. 
+	// (www.manning.com/bjarnason/)
+		def double(rng: RNG): (Double, RNG) = {
+		val (r, _) = rng.nextInt
+		val d: Double = ( math.abs(r).toDouble / Int.MaxValue ) match {
+			case 1 => .99999
+			case x: Double => x
+		}
+		(d, rng)
+		}
 
 	// EXERCISE 1: Write a function that uses RNG.nextInt to generate 
 	// a random positive integer. Make sure to handle the corner case 
@@ -44,5 +51,24 @@ object Test {
 			case _ => r
 		}
 		(posR, rng2)
+	}
+
+	// EXERCISE 3: Write functions to generate an (Int, Double) pair, a
+	// (Double, Int) pair, and a (Double, Double, Double) 3-tuple. You
+	// should be able to reuse the functions you've already written
+	def intDouble(rng: RNG): ((Int,Double), RNG) = {
+		val (i: Int, rng2) = rng.nextInt
+		val (d: Double, _) = double(rng)
+		((i,d), rng2)
+	}
+
+	// EXERCISE 4: Write a function to generate a list of random integers.
+	def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+		def go(count: Int, currentRng: RNG, acc: List[Int]) : (List[Int], RNG) = count match {
+			case 0 => (acc, currentRng)
+			case _ => val (r, rng2) = currentRng.nextInt
+			          go(count - 1, rng2, acc :+ r)
+		}
+		go(count, rng, List[Int]())
 	}
 }
