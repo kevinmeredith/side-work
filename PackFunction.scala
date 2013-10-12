d5ff2fab34958f7064d734e35f609bfad073c707
kman007us <kevin.m.meredith@gmail.com>
2013-10-11 22:46:01 -0400

    Added PackFunction.scala

---
+++ PackFunction.scala
@@ -0,0 +1,20 @@
+object PackFunction {
+	
+	// Put duplicates into a separate list
+	def pack[T](xs: List[T]): List[List[T]] = {
+		def go[T](ys: List[T], acc: List[List[T]]) : List[List[T]] = ys match {
+			case Nil => acc
+			//case x :: xs_ if(x.isEmpty) => go(xs_, acc)
+			case x :: xs_ => val r: List[T] = ys.takeWhile(a => a == x)
+							 go(ys.drop(r.length), acc :+ r)
+		}
+		go(xs, List(Nil).filter(_ != Nil))
+	}
+
+	def main(args: Array[String]) = {
+		val list = List("a", "a", "a", "b", "b", "c", "c", "c", "a")
+		val r = pack(list)
+		println("r : " + r)
+	}
+
+}
