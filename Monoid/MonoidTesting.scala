2fce9330b4fdb4c1ee83be86e47a1af5465884ad
kman007us <kevin.m.meredith@gmail.com>
2013-10-21 21:15:17 -0400

    more exercises

--- Monoid/MonoidTesting.scala
+++ Monoid/MonoidTesting.scala
@@ -4,8 +4,19 @@ object MonoidTesting {
 
 		val words = List("Hic", "Est", "Barbarus")
 
-		println("foldLeft: " + words.foldLeft(stringMonoid.zero)(stringMonoid.op))
-		println("foldRight: " + words.foldRight(stringMonoid.zero)(stringMonoid.op))
+		val fl = words.foldLeft(stringMonoid.zero)(stringMonoid.op)
+		println("fl : " + fl)
+		assert(fl == "HicEstBarbarus")
+
+		val fr = words.foldRight(stringMonoid.zero)(stringMonoid.op)
+		println("fr: " + fr)
+		assert(fr == "HicEstBarbarus")
+		
+		val res = trimMonoid.op( ("Hic"), (trimMonoid.op("est ", "chorda ")) )
+		println("res : " + res)
+		assert(res == "Hic est chorda")
+
+		println("success")
 	}
 
 	trait Monoid[A] {
@@ -44,13 +55,34 @@ object MonoidTesting {
 		def op(a1: Boolean, a2: Boolean) = a1 || a2
 		val zero = false
 	}
+
+
+	// EXERCISE 5: Write a monoid instance for that String inserts spaces
+	// between words unless there already is one, and trims spaces off the ends of the
+	// result.
+	def trimMonoid = new Monoid[String] {
+		def op(a1: String, a2: String) = a1.trim + " " + a2.trim
+		val zero = ""
+	}
 	
+	//def trimMonoid(s: String): Monoid[String] = {
+
 	// EXERCISE 2: Give a Monoid instance for combining Options:
-	/*def optionMonoid[A] = new Monoid[Option[A]] {
+	/*def optionMonoid[A](implicit aMonoid:Monoid[A]) = new Monoid[Option[A]] {
 		def op(a1: Option[A], a2: Option[A]) = (a1, a2) match {
-			case (Some(x), Some(y)) => Some(x + y)
+			case (Some(x), Some(y)) => Some(aMonoid.op(x, y))
 			case _ => None
 		}
-		val zero = None
+		val zero = // op(x, zero) === x === op(zero, x) 
 	}*/
+
+	// EXERCISE 6: Implement concatenate, a function that folds a list with a
+	// monoid:
+	def concatenate[A](as: List[A], m: Monoid[A]): A = {
+		def go(xs: List[A], acc: A): A = xs match {
+			case y :: ys => go(ys, m.op(acc, y))
+			case Nil => acc
+		}
+		go(as, Nil)
+	}
 }
