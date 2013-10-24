6580c0b708d79efdd992115a285b4ae26c0f5631
kman007us <kevin.m.meredith@gmail.com>
2013-10-23 23:01:55 -0400

    more exercises with Monoids

--- Monoid/MonoidTesting.scala
+++ Monoid/MonoidTesting.scala
@@ -30,10 +30,27 @@ object MonoidTesting {
 		assert(strs == "123")
 
 		//def foldMapRight[A, B](as: List[A], m: Monoid[B])(z: B)(f: (A, B) => B): B = {		
-		val stringInts = List("1", "2", "3")
+		/*val stringInts = List("1", "2", "3")
 		val resFoldMap = foldMapRight(stringInts, intAddition)(intAddition.zero)((s, i) => intAddition.op(s.toInt, i))
 		println("resFoldMap: " + resFoldMap)
-		assert(resFoldMap == 6)
+		assert(resFoldMap == 6)*/
+
+		// testing foldMapV
+		val seq = IndexedSeq("1","2","3","4")
+		val r = foldMapV(seq, intAddition)(x => x.toInt)
+		println(r)
+		assert(r == 10)
+
+		// testing foldMap if IndexedSeq[Int] is ordered
+		val seqInt = IndexedSeq("1","2","3","4")
+		val isOrdered = foldMapV(seqInt, ordered)(x => x.toInt)
+		println(isOrdered)
+		assert(isOrdered == true)
+
+		val seqInt2 = IndexedSeq("4", "3", "2", "1")
+		val isOrdered2 = foldMapV(seqInt2, ordered)(x => x.toInt)
+		println(isOrdered2)
+		assert(isOrdered2 == false)
 
 		println("success")
 	}
@@ -42,6 +59,14 @@ object MonoidTesting {
 		def op(a1: A, a2: A): A
 		def zero: A
 	}
+
+	// EXERCISE 10 (hard): Use to detect foldMap whether a given
+	// IndexedSeq[Int] is ordered. You will need to come up with a creative
+	// Monoid.
+	/*val ordered = new Monoid[Boolean] = {
+		def op(a1: Int, a2: Int) = if(a1 < a2) true else false
+		val zero = true
+	}*/
 
 	val stringMonoid = new Monoid[String] {
 		def op(a1: String, a2: String) = a1 + a2
@@ -84,8 +109,6 @@ object MonoidTesting {
 		val zero = ""
 	}
 	
-	//def trimMonoid(s: String): Monoid[String] = {
-
 	// EXERCISE 2: Give a Monoid instance for combining Options:
 	/*def optionMonoid[A](implicit aMonoid:Monoid[A]) = new Monoid[Option[A]] {
 		def op(a1: Option[A], a2: Option[A]) = (a1, a2) match {
@@ -116,11 +139,26 @@ object MonoidTesting {
 	// foldLeft or foldRight. But you can also write foldLeft and foldRight
 	// using foldMap! Try it.
 	// def foldRight[B](z: B)(op: (A, B) => B): B
-	def foldMapRight[A, B](as: List[A], m: Monoid[B])(z: B)(f: (A, B) => B): B = {
-		def go(ys: List[A], acc: B) : B = ys match {
-			case x :: xs => go(xs, f(x, acc))
+	/*def foldMapRight[A, B](as: List[A], m: Monoid[B])(f: A => B): B
+		foldMap(as, m,)
+	}*/
+
+	// foldRight: op(a, op(b, op(c, d)))
+	// foldLeft: op(op(op(a b) c) d)
+
+	// EXERCISE 9: Implement a foldMap for IndexedSeq, a common supertype
+	// for various data structures that provide efficient random access including Vector.
+	// Your implementation should use the strategy of splitting the sequence in two,/
+	// recursively processing each half, and then adding the answers together with the
+	// monoid.
+	def foldMapV[A,B](v: IndexedSeq[A], m: Monoid[B])(f: A => B): B = {
+		def go(seq: List[B], acc: B): B = seq match {
+			case x :: xs => go(xs, m.op(acc, x))
 			case Nil => acc
 		}
-		go(as, z)
+
+		val as: List[B] = v.map(f).toList
+		val (a: List[B], b: List[B]) = as.splitAt(as.length / 2)
+		m.op(go(a, m.zero), go(b, m.zero))
 	}
 }
