e4829724e406b91c8f567af4169c1650708aa6dd
kman007us <kevin.m.meredith@gmail.com>
2013-11-03 15:02:23 -0500

    added indexedseq functions

--- Foldable/Foldable.scala
+++ Foldable/Foldable.scala
@@ -26,6 +26,29 @@ object ListFoldable extends Foldable[List] {
 	override def concatenate[A](as: List[A])(m: Monoid[A]): A = as.foldLeft(m.zero)(m.op)
 }
 
+object IndexedSeqFoldable extends Foldable[IndexedSeq] {
+  override def foldRight[A, B](as: IndexedSeq[A])(z: B)(f: (A, B) => B): B = sys.error("TODO")
+  override def foldLeft[A, B](as: IndexedSeq[A])(z: B)(f: (B, A) => B): B = {
+  	def go(bs: IndexedSeq[A], acc: B): B = {
+  		if (bs.isEmpty) acc
+  		else go(bs.tail, f(acc, bs.head))
+  	}
+	go(as, z)
+  }
+
+  def foldLeftPM[A, B](as: IndexedSeq[A])(z: B)(f: (B, A) => B): B = {
+  	def go(bs: IndexedSeq[A], acc: B): B = bs match {
+  		case x +: xs => println("x: " + x + "; xs: " + xs)
+  						go(xs, f(acc, x))
+  		case _ => acc
+  	}
+  	go(as, z)
+  }
+
+  override def foldMap[A, B](as: IndexedSeq[A])(f: A => B)(mb: Monoid[B]): B = sys.error("TODO")
+  override def concatenate[A](as: IndexedSeq[A])(m: Monoid[A]): A = sys.error("TODO")
+}
+
 object FoldableTest { 
 	
 	val intAddition = new Monoid[Int] {
@@ -40,8 +63,10 @@ object FoldableTest {
 		assert(ListFoldable.foldRight(list)(0)((s, i) => s + i) == 15)
 		assert(ListFoldable.foldLeft(list)(0)((i, s) => i + s) == 15)
 		assert(ListFoldable.foldMap(listStr)(x => x.toInt)(intAddition) == 15)
-		//assert(ListFoldable.concatenate())
-
+		
+		val indexedSeq = IndexedSeq(1,2,3)
+		assert(IndexedSeqFoldable.foldLeft(indexedSeq)(0)((i, s) => i + s) == 6)
+		assert(IndexedSeqFoldable.foldLeftPM(indexedSeq)(0)((i, s) => i + s) == 6)
 		println("success")
 	}
 }
