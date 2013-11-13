8ecc011ba6c57d755a4fe88d069642d40cc34565
kman007us <kevin.m.meredith@gmail.com>
2013-11-13 03:07:13 +0000

    added work to figure out if Try is a Monad & Try.scala

---
+++ Monads/Try.scala
@@ -0,0 +1,15 @@
+abstract class Try[T] {
+	def flatMap[U](f: T => Try[U]): Try[U] this match {
+		case Success(x) => try f(x) catch { case NonFatal(ex) => Failure(ex) }
+		case fail: Failure => fail
+	}
+
+	def map[U](f: T => U): Try[U] = this match {
+		case Success(x) => Try(f(x))
+		case fail: Failure => fail
+	}
+
+
+	// t map f == t flatMap (x => Try(f(x))
+	//  	   == t flatMap (f andThen Try)
+}
