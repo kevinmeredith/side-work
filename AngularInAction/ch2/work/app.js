4f6417f1fe96e0c96f65201704bf4a2d92ab1939
kman007us <kevin.m.meredith@gmail.com>
2013-11-16 00:10:55 -0500

    continued work on adding 'add story' button

--- AngularInAction/ch2/work/app.js
+++ AngularInAction/ch2/work/app.js
@@ -1,6 +1,22 @@
 var myModule = angular.module('Angello', []);
 
 myModule.controller('MainCtrl', function($scope) { 
+	$scope.currentStory;
+	$scope.newStoryTitle;
+	$scope.newStoryDesc;
+
+	$scope.setCurrentStory = function(story) {
+		$scope.currentStory = story
+	};
+
+	$scope.getNewStoryTitle = function() { 
+		return $scope.newStoryTitle;
+	};
+
+	$scope.getNewStoryDesc = function() { 
+		return $scope.newStoryDesc;
+	};
+
 	$scope.stories = [
 			{title: 'Story 00', description: 'Description pending.'},
 			{title: 'Story 01', description: 'Description pending.'},
@@ -9,4 +25,11 @@ myModule.controller('MainCtrl', function($scope) {
 			{title: 'Story 04', description: 'Description pending.'},
 			{title: 'Story 05', description: 'Description pending.'}
 		];
-	});
+
+	$scope.createStory = function(t, d) { 
+		console.log("in create story");
+		const newStory = [{title: t, description: d}];
+		console.log("adding newStory: " + JSON.stringify(newStory));
+		$scope.stories = Array.prototype.concat($scope.stories, newStory);
+	};
+});
