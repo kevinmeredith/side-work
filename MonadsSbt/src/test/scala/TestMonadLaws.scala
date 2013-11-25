e9e0497be5a49362ca3aa3e68e5a3ce959291b91
kman007us <kevin.m.meredith@gmail.com>
2013-11-24 23:01:02 -0500

    added monad test: for-expression to flatMap/map

---
+++ MonadsSbt/src/test/scala/TestMonadLaws.scala
@@ -0,0 +1,37 @@
+import org.scalacheck.Gen
+import org.scalatest._
+
+class TestMonadLaws extends FlatSpec {
+	
+case class Order(item: Item, quantity: Int)
+case class Item(name: String, price: Double)
+
+val str = List("foo")
+
+	"the for expression" should "match the map" in {
+		val genOrder: Gen[Order] = for {
+			name <- str
+			price <- Gen.choose(1, 100).map(_ * 10)
+			quantity <- Gen.choose(1,100)
+		} yield Order(Item(name, price), quantity)
+		println("for-expression: " + genOrder)
+	}
+
+	"the map representation" should "match the map" in {
+		val genOrder: Gen[Order] = str.flatMap(name => Gen.uniform.map(_ * 10)
+												  .map(price => Gen.choose(1, 100)
+												  .map(quantity => Order(Item(name, price), quantity))))
+
+		println("map " + genOrder)
+	}
+	/*
+	val genItem: Gen[Item] = for {
+		name <- Gen.stringN(3)
+		price <- Gen.uniform.map(_ * 10)
+	} yield Item(name, price)
+
+	val genOrder2: Gen[Order] = for {
+		item <- genItem
+		quantity <- Gen.choose(1,100)
+	} yield Order(item, quantity)*/
+}
