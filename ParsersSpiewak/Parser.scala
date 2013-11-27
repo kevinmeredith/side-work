8b2cffb2389ca3f2f5cff5c8e93f537e36a4f05b
kman007us <kevin.m.meredith@gmail.com>
2013-11-26 21:55:23 -0500

    added comments for explanation and added ~> and <~

--- ParsersSpiewak/Parser.scala
+++ ParsersSpiewak/Parser.scala
@@ -1,5 +1,53 @@
 //@author dspiewak 
-trait Parser[+A] extends (Stream[Character]=>Result[A])
+class SequenceParser[+A, +B](l: =>Parser[A],
+							 r: =>Parser[B]) extends Parser[(A, B)] {
+	lazy val left = l      // "foo"
+	lazy val right = r     // "baz"
+
+	def apply(s: Stream[Character]) = left(s) match { // Stream('f','o','o','b','a','r')
+		case Success(a, rem) => right(rem) match { // Stream('b','a','r')
+			case Success(b, rem) => Success((a,b), rem) // Failure("got bar, expected baz")
+			case f: Failure => f
+		}
+		case f: Failure => f
+	}
+}
+
+// Disjunctive Combinator (OR logic)
+class DisParser[+A](left: Parser[A], right: Parser[A]) extends Parser[A] {
+  def apply(s: Stream[Character]) = left(s) match {
+    case res@Success(_, _) => res
+    case _: Failure => right(s)
+  }
+}
+
+// ~> is a parser combinator for sequential composition which keeps only the right result.
+// @author kmeredith
+class KeepRightParser[+A](left: =>Parser[A], 
+	                     right: =>Parser[A]) extends Parser[A] {
+	def apply(s: Stream[Character]) = left(s) match {
+		case Success(_, rem) => right(rem)
+		case f: Failure => f
+	}
+}
+
+// <~ is a parser combinator for sequential composition which keeps only the left result.
+// @author kmeredith
+class KeepLeftParser[+A](left: =>Parser[A], 
+	                     right: =>Parser[A]) extends Parser[A] {
+	def apply(s: Stream[Character]) = left(s) match {
+		case Success(a, rem) => right(rem) match { 
+			case Success(_, _) => Success(a, rem)
+			case f: Failure => f
+		}
+		case f: Failure => f
+	}
+}
+
+trait Parser[+A] extends (Stream[Character]=>Result[A]) {
+	def ~[B](that: =>Parser[B]) = new SequenceParser(this, that)
+	def |[AA >: A](that: Parser[AA]) = new DisParser(this, that)
+}
 
 sealed trait Result[+A]
 
@@ -8,44 +56,29 @@ case class Success[+A](value: A, rem: Stream[Character]) extends Result[A]
 case class Failure(msg: String) extends Result[Nothing]
 
 object RegexpParsers {
-	 implicit def keyword(str: String) = new Parser[String] {
-		def apply(s: Stream[Character]) = {
-			val trunc = s take str.length
+	 implicit def keyword(str: String) = new Parser[String] { 
+		def apply(s: Stream[Character]) = { // s = Stream('f','o','o','b','a','r')
+			val trunc = s take str.length // parser(str) = foo
 			lazy val errorMessage = "Expected '%s' got '%s'".format(str, trunc.mkString)
 
 			if (trunc.lengthCompare(str.length) != 0)
 				Failure(errorMessage)
 			else {
-				val succ = trunc.zipWithIndex forall {
-					case (c, i) => c == str(i)
+				val succ = trunc.zipWithIndex forall { // truncStr = Vector( (f,0), (o,1), (o,2))
+					case (c, i) => c == str(i)         // parser = "foo"
 				}
 
-				if(succ) Success(str, s drop str.length)
+				if(succ) Success(str, s drop str.length) // Success(foo, Stream('b','a','r'))
 				else Failure(errorMessage)
 			}
 		}
 	}
-
-class SequenceParser[+A, +B](l: =>Parser[A],
-							 r: =>Parser[B]) extends Parser[(A, B)] {
-	lazy val left = l
-	lazy val right = r
-
-	def apply(s: Stream[Character]) = left(s) match {
-		case Success(a, rem) => right(rem) match {
-			case Success(b, rem) => Success((a,b), rem)
-			case f: Failure => f
-		}
-		case f: Failure => f
-	}
-}
-
-
+	//@author kmeredith examples of above code
 	def main(args: Array[String]) = {
 		//testing simple parser
 		val s: Stream[Character] = Stream('f', 'o', 'o')
 		val p1 = keyword("foo")
-		println("p1(s): " + p1(s))
+		println("p1(s): " + p1.apply(s))
 
 		val s2: Stream[Character] = Stream('f', 'o', 'o', 'b', 'a', 'r')
 		val p2 = keyword("bar")
@@ -54,5 +87,20 @@ class SequenceParser[+A, +B](l: =>Parser[A],
 		// testing parser sequence
 		val seqPar = new SequenceParser(p1, p2)
 		println("seqPar: " + seqPar(s2))
+
+		// Using the ~ operator where "foo" and "bar" are two Parser Combinators
+		// note that, I believe you can pass only a string due to the `keyword` implicit
+		println("AND : " + ("foo" ~ "bar")(s2))
+
+		val s3: Stream[Character] = Stream('f', 'o', 'o', 'b', 'a', 'z')
+		println("OR :" + ("bar" | "baz")(s3)) 
+		println("OR :" + ("bar" | "foo")(s3)) 
+
+		val s4 = Stream[Character]('f', 'o', 'o', 'b', 'a', 'r', 'b', 'u', 'z', 'z')
+		val krp = new KeepRightParser("foo", "bar")
+		println("~> test: " + krp(s4))
+
+		val klp = new KeepLeftParser("foo", "bar")
+		println("<~ test: " + klp(s4))
 	}
 }
