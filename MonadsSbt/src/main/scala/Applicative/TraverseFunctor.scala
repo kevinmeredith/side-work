c0ae20ca2376f52f42a234b9757662ce0334d3e1
kman007us <kevin.m.meredith@gmail.com>
2014-01-27 22:54:05 -0500

    removed hard exercise attempt since I reviewed the answer from pchiusano's blog

--- MonadsSbt/src/main/scala/Applicative/TraverseFunctor.scala
+++
@@ -1,9 +0,0 @@
-package Applicative
-
-import Functor.Functor
-
-// Exercise 13(hard): Implement map in terms of traverse for an arbitrary Traverse[F].
-trait TraverseFunctor[F[_]] extends Functor[F] {
-  def traverse[G[_],A,B](fa: F[A])(f: A => G[B])(implicit G: Applicative[G]): G[F[B]] =
-    sequence(map(fa)(f))
-}
