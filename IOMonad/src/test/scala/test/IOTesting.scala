e997ef44d768083deea738516d4b50d75830f00a
kman007us <kevin.m.meredith@gmail.com>
2014-02-10 22:53:24 -0500

    IOMonad\src\main\scala\common\IO1.scala

--- IOMonad/src/test/scala/test/IOTesting.scala
+++ IOMonad/src/test/scala/test/IOTesting.scala
@@ -1,10 +1,31 @@
 package test
 
-import common._
+import common.IO1.IO
+import common.IO1.Util._
 import org.scalatest._
 
 class IOTesting extends FlatSpec {
 
+  "Testing `echo`" should "show a user's input" in {
+    val echo = ReadLine.flatMap(PrintLine)
+    println("enter text")
+    echo.run
+  }
 
+  "Read an Int" should "read a user's input and convert to an Int" in {
+    val readInt = ReadLine.map(x => {println("_.toInt"); x.toInt})
+    println("enter an int")
+    readInt.run
+  }
 
+  "the user should be prompted for two inputs" should "print out their product" in {
+    val readInts: Int = readInt * readInt
+    println("readInts:" + readInts)
+
+  }
+
+  "calling `PrintLine('hello world')`" should "print out 'hello world'" in {
+    val printLine = PrintLine("hello world")
+    printLine.run
+  }
 }
